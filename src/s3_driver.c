d5c13acdc525296f3dd7b7fc99737be2423c1a01
Adam Jackson <ajax@nwnk.net>
2004-07-30 20:30:54 +0000

    Bug #400 (partial): Driver fixes for the dlloader. When using dlloader, all     framebuffer formats except cfb and the overlay modes should work, and     r128 and radeon need to be loaded from the ati driver (both issues to     be fixed soon). Tested on i740, s3virge, mach64, tdfx, vesa, and vga     drivers. elfloader users shouldn't be affected.

--- src/s3_driver.c
+++ src/s3_driver.c
@@ -34,7 +34,7 @@
  *
  *
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/s3/s3_driver.c,v 1.18 2003/08/25 18:44:36 eich Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/s3/s3_driver.c,v 1.19tsi Exp $ */
 
 
 #include "xf86.h"
@@ -171,6 +171,8 @@ static OptionInfoRec S3Options[] = {
 RamDacSupportedInfoRec S3IBMRamdacs[] = {
 	{ IBM524_RAMDAC },
 	{ IBM524A_RAMDAC },
+	{ IBM526_RAMDAC },
+	{ IBM526DB_RAMDAC },
 	{ -1 }
 };
 
@@ -256,7 +258,7 @@ static XF86ModuleVersionInfo S3VersRec = {
         MODULEVENDORSTRING,
         MODINFOSTRING1,
         MODINFOSTRING2,
-        XF86_VERSION_CURRENT,
+        XORG_VERSION_CURRENT,
         VERSION_MAJOR, VERSION_MINOR, PATCHLEVEL,
         ABI_CLASS_VIDEODRV,
         ABI_VIDEODRV_VERSION,
@@ -544,13 +546,16 @@ static Bool S3PreInit(ScrnInfoPtr pScrn, int flags)
 	}
 
 	pS3->FBAddress = pS3->PciInfo->memBase[0];
+	pScrn->memPhysBase = pS3->FBAddress;
+	pScrn->fbOffset = 0;
+	
 	if (pS3->S3NewMMIO)
 		pS3->IOAddress = pS3->FBAddress + S3_NEWMMIO_REGBASE;
 
-	xf86DrvMsg(pScrn->scrnIndex, X_PROBED, "Framebuffer @ 0x%x\n",
+	xf86DrvMsg(pScrn->scrnIndex, X_PROBED, "Framebuffer @ 0x%lx\n",
 		   pS3->FBAddress);
 	if (pS3->S3NewMMIO)
-		xf86DrvMsg(pScrn->scrnIndex, X_PROBED, "MMIO @ 0x%x\n",
+		xf86DrvMsg(pScrn->scrnIndex, X_PROBED, "MMIO @ 0x%lx\n",
 			   pS3->IOAddress);
 
 	pS3->PCIRetry = FALSE;		/* not supported yet */
@@ -657,7 +662,7 @@ static Bool S3PreInit(ScrnInfoPtr pScrn, int flags)
 		pS3->DacSave = S3IBMRGB_Save;
 		pS3->DacRestore = S3IBMRGB_Restore;
 		pS3->CursorInit = S3IBMRGB_CursorInit;
-		pS3->RamDac->SetBpp = IBMramdac526SetBpp;
+		pS3->RamDac->SetBpp = LoaderSymbol("IBMramdac526SetBpp");
 		pS3->MaxClock = 170000;
 		pScrn->rgbBits = 8;
 		pS3->LoadPalette = S3GenericLoadPalette;
@@ -953,14 +958,9 @@ static void S3Save(ScrnInfoPtr pScrn)
 	S3RegPtr save = &pS3->SavedRegs;
 	vgaHWPtr hwp = VGAHWPTR(pScrn);
         vgaRegPtr pVga = &hwp->SavedReg;
-	RamDacHWRecPtr pRAMDAC;
-	RamDacRegRecPtr RAMDACreg;
 	int vgaCRIndex = pS3->vgaCRIndex, vgaCRReg = pS3->vgaCRReg;
 	int i;
 	unsigned char cr5c = 0;
-
-	pRAMDAC = RAMDACHWPTR(pScrn);
-	RAMDACreg = &pRAMDAC->SavedReg;
 
 	S3BankZero(pScrn);
 
