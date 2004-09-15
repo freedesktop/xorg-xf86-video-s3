19b52aa1dad10b98343d806595b26004e0bd863d
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-09-15 16:34:02 +0000

    Pull XORG-6_8_0 to CYGWIN branch

--- src/s3_accel.c
+++ src/s3_accel.c
@@ -24,7 +24,7 @@
  *
  *
  */
-/* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/s3/s3_accel.c,v 1.1.4.1.4.2 2004/03/04 20:16:30 kaleb Exp $ */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/s3/s3_accel.c,v 1.2 2004/04/23 19:43:14 eich Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/s3/s3_accel.c,v 1.2 2001/10/28 03:33:44 tsi Exp $ */
 
 
